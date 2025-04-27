1db81fb2b781f27091986e53be873875b2b53d9b
itsepsita <77191477+itsepsita@users.noreply.github.com>
2025-04-27 19:18:03 +0530

    Create ApiGatewayServiceApplication.java

---
+++ GatewayService/src/main/java/com/db/service/ApiGatewayServiceApplication.java
@@ -0,0 +1,17 @@
+package com.db.APIGatewayService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayServiceApplication.class, args);
+
+	}
+	
+}
+
+
+
